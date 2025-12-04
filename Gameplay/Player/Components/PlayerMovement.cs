719914effac38ce44370ae576cd6ad1db690a22e
romainjbr <romainjbrafai@gmail.com>
2025-12-04 13:35:46 +0100

    Implement Move() method in PlayerMovement

--- Gameplay/Player/Components/PlayerMovement.cs
+++ Gameplay/Player/Components/PlayerMovement.cs
@@ -1,8 +1,27 @@
+using Skogen.Gameplay.Player;
 using UnityEngine;
 
 namespace Skogen.Gameplay.Player.Components
 {
     public class PlayerMovement : MonoBehaviour
     {
+        private PlayerController player;
+
+        public PlayerMovement(PlayerController player)
+        {
+            this.player = player;
+        }
+
+        public void Move(Vector2 input)
+        {
+            if (player.Context.References.RigidBody == null) { return; }
+
+            var direction = input.x;
+            player.Context.Stats.Direction = input;
+
+            var rb = player.Context.References.RigidBody;
+            var velocity = new Vector2(direction * player.Context.Stats.WalkSpeed, rb.linearVelocity.y);
+            rb.linearVelocity = velocity;
+        }
     }
 }
