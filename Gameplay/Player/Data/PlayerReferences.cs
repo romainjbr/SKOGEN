c02e0b32fb8bc32dfc83bb9d6fe2824af82035c4
romainjbr <romainjbrafai@gmail.com>
2025-12-04 10:07:08 +0100

    Add Player's RidigBody and Collider in PlayerReferences

--- Gameplay/Player/Data/PlayerReferences.cs
+++ Gameplay/Player/Data/PlayerReferences.cs
@@ -1,3 +1,4 @@
+using Skogen.Gameplay.Player;
 using UnityEngine;
 
 namespace Skogen.Gameplay.Player.Data
@@ -5,6 +6,27 @@ namespace Skogen.Gameplay.Player.Data
     [System.Serializable]
     public class PlayerReferences
     {
+        [SerializeField] private Rigidbody2D rigidBody;
+        [SerializeField] private Collider2D collider;
+
+        public Rigidbody2D RigidBody => rigidBody;
+        public Collider2D Collider => collider;
+
+        private PlayerController player;
         
+        public void Initialize(PlayerController player)
+        {
+            this.player = player;
+
+            if (rigidBody == null)
+            {
+                rigidBody = player.GetComponent<Rigidbody2D>();
+            }
+
+            if (collider == null)
+            {
+                collider = player.GetComponent<Collider2D>();
+            }
+        }
     }
 }
