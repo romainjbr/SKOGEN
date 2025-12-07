650f162428ab143f7715582adce7770e287675b3
romainjbr <romainjbrafai@gmail.com>
2025-12-07 20:42:26 +0100

    Create health properties and implement Heal() method

--- Gameplay/Player/Components/PlayerHealth.cs
+++ Gameplay/Player/Components/PlayerHealth.cs
@@ -4,5 +4,25 @@ namespace Skogen.Gameplay.Player.Components
 {
     public class PlayerHealth : MonoBehaviour
     {
+        [SerializeField] private int maxHealth = 3;
+        [SerializeField] private int currentHealth;
+
+        private PlayerController player;
+
+        public int MaxHealth => maxHealth;
+        public int CurrentHealth => currentHealth;
+
+        public PlayerHealth(PlayerController player)
+        {
+            this.player = player;
+            currentHealth = maxHealth;
+        }
+
+        public void Heal(int amount)
+        {
+            if (amount <= 0 || (amout + currentHealth < maxHealth)) { return; }
+
+            currentHealth += amount;
+        }
     }
 }
