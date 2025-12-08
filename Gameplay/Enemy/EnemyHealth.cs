c25621d4735ef77d0a270703b869fc7ac4f04f7e
romainjbr <romainjbrafai@gmail.com>
2025-12-08 10:21:09 +0100

    Declare EnemyHealth properties

--- Gameplay/Enemy/EnemyHealth.cs
+++ Gameplay/Enemy/EnemyHealth.cs
@@ -5,6 +5,17 @@ namespace Skogen.Gameplay.Enemy
 {
     public class EnemyHealth : MonoBehaviour
     {
-        
+        [Header("Health")]
+        [SerializeField] private int maxHealth = 3;
+        private int currentHealth;
+
+        public int MaxHealth => maxHealth;
+        public int CurrentHealth => currentHealth;
+
+        public void Initialize(EnemyController controller)
+        {
+            this.controller = controller;
+            currentHealth = maxHealth;
+        }
     }
 }
