1c7eea32a6eb3f3b1fbd89871b5ccb91bdcda939
romainjbr <romainjbrafai@gmail.com>
2025-12-08 11:21:03 +0100

    Add Projectile.cs and shooting feature in EnemyCombat.cs

--- Gameplay/Enemy/EnemyCombat.cs
+++ Gameplay/Enemy/EnemyCombat.cs
@@ -10,8 +10,15 @@ namespace Skogen.Gameplay.Enemy
         [SerializeField] private int contactDamage = 1;
         [SerializeField] private float contactDamageCooldown = 2f;
 
+        [Header("Shooting")]
+        [SerializeField] private Transform shootFromPoint;
+        [SerializeField] private GameObject projectilePrefab;
+        [SerializeField] private float projectileSpeed = 10f;
+        [SerializeField] private float shootCooldown = 3f;
+
         private EnemyController controller;
 
+        private float timeSinceLastShot = 0f;
         private float timeSinceLastContactDamage = 0f;
 
         public EnemyCombat(EnemyController controller)
@@ -43,10 +50,29 @@ namespace Skogen.Gameplay.Enemy
             }
         }
 
+        public void OnShootAreaEnter(Transform playerTransform)
+        {
+            if (timeSinceLastShot < shootCooldown) { return; }
+            ThrowProjectile(playerTransform);
+
+            timeSinceLastShot = 0f;
+        }
+
         private void DamagePlayer(GameObject playerObject, int damage)
         {
             var player = playerObject.GetComponent<PlayerController>();
             player?.Context?.Health?.TakeDamage(damage);
+        }
+
+        private void ThrowProjectile(Transform target)
+        {
+            if (projectilePrefab == null || shootFromPoint == null || target == null) { return; }
+
+            var projectile = Instantiate(projectilePrefab, shootFromPoint.position, Quaternion.identity);
+            var direction = (target.position - projectile.transform.position).normalized;
+
+            var projectileComp = projectile.GetComponent<Projectile>();
+            projectileComp?.Move(direction * projectileSpeed);
         }
     }
 }
