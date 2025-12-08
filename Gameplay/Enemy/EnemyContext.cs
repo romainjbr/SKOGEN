7233955b469e041a65d2801c1dcef4f914405a34
romainjbr <romainjbrafai@gmail.com>
2025-12-08 10:19:41 +0100

    Add EnemyHealth class and add it to EnemyContext

--- Gameplay/Enemy/EnemyContext.cs
+++ Gameplay/Enemy/EnemyContext.cs
@@ -5,11 +5,15 @@ namespace Skogen.Gameplay.Enemy
     [System.Serializable]
     public class EnemyContext
     {
+        public EnemyHealth Health { get; set; }
+        
         private EnemyController enemy;
 
         public EnemyContext(EnemyController enemy)
         {
             this.enemy = enemy;
+
+            if (Health == null) { Health = new EnemyHealth(enemy); }
         }
     }
 }
---
+++ Gameplay/Enemy/EnemyHealth.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace Skogen.Gameplay.Enemy
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        
+    }
+}
