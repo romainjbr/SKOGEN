76d9bc583fd66e2be922a291eafc4d4f52ee37c9
romainjbr <romainjbrafai@gmail.com>
2025-12-11 18:54:11 +0100

    Implement Push actions in PlayerActions.cs

--- Gameplay/Player/Components/PlayerActions.cs
+++ Gameplay/Player/Components/PlayerActions.cs
@@ -11,5 +11,28 @@ namespace Skogen.Gameplay.Player.Components
         {
             this.player = player;
         }
+        
+        public void SetPushable(PushableObject pushable)
+        {
+            player.Context.References.CurrentPushable = pushable;
+        }
+
+        public void ClearPushable(PushableObject pushable)
+        {
+            if (player.Context.References.CurrentPushable == pushable)
+            {
+                player.Context.References.CurrentPushable = null;
+            }
+        }
+
+        // TODO: Implement animation
+        public void StartPush()
+        {
+        }
+
+        // TODO: Implement animation
+        public void StopPush()
+        {       
+        }
     }
 }
